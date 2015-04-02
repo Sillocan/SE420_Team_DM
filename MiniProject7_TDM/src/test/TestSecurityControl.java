dfe9c1c535b95aae8ad394d32cd3208479140ab8
Chris Silvano <chris.silvano@hotmail.com>
2015-04-02 16:31:57 -0400

    Initial creation of test package and TestSecurityControl.java for JUnit test cases.

---
+++ MiniProject7_TDM/src/test/TestSecurityControl.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import SecurityLightController.*;
+import UI.*;
+//how to import the default package??
+
+/** This class will house all JUnit test cases for the
+ *  Security Light application
+ *  @author Chris Silvano, Alex Spradlin, Casey Layne */
+public class TestSecurityControl {
+
+}
