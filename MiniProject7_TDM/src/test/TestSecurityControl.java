e93e98a751f91090eb95bedf61376c3533932c29
Chris Silvano <chris.silvano@hotmail.com>
2015-04-03 10:11:46 -0400

    Removed comment asking about importing default package into JUnit test case class.

--- MiniProject7_TDM/src/test/TestSecurityControl.java
+++ MiniProject7_TDM/src/test/TestSecurityControl.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import SecurityLightController.*;
 import UI.*;
-//how to import the default package??
 
 /** This class will house all JUnit test cases for the
  *  Security Light application
