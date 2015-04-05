5f2b166c6fecd7e767b68a0f486fdaf0be507aac
Chris Silvano <chris.silvano@hotmail.com>
2015-04-05 13:06:10 -0400

    Added framework for Chris S. JUnit test cases.

--- MiniProject7_TDM/src/test/TestSecurityControl.java
+++ MiniProject7_TDM/src/test/TestSecurityControl.java
@@ -1,7 +1,9 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
 import SecurityLightController.*;
 import UI.*;
 
@@ -10,4 +12,36 @@ import UI.*;
  *  @author Chris Silvano, Alex Spradlin, Casey Layne */
 public class TestSecurityControl {
 
+	//used to determine which state you are in
+	LightControllerStateMachineObserverInterface lightStateMachine;
+	LightControllerCommandInterface lightControllerCommand;
+	
+	/** This method
+	 *  - author Chris Silvano */
+	@Test 
+	public void securityAlarmTrippedNight() {
+
+	}
+	
+	/** This method
+	 *  - author Chris Silvano */
+	@Test 
+	public void securityAlarmTrippedMotion() {
+
+	}
+	
+	/** This method
+	 *  - author Chris Silvano */
+	@Test 
+	public void alarmClearedLampOn() {
+
+	}
+	
+	/** This method
+	 *  - author Chris Silvano */
+	@Test 
+	public void alarmClearedLampOff() {
+
+	}
+		
 }
