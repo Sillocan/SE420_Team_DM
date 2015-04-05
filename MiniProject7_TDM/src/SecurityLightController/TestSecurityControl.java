7631179684d414f25c89059f05ef90e89303c542
Chris Silvano <chris.silvano@hotmail.com>
2015-04-05 14:16:00 -0400

    Moved TestSecurityControl into src.SecurityLightController package due to some methods having protected characteristic for testing.

---
+++ MiniProject7_TDM/src/SecurityLightController/TestSecurityControl.java
@@ -0,0 +1,48 @@
+package src.SecurityLightController;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import src.SecurityLightController.*;
+import src.UI.*;
+
+/** This class will house all JUnit test cases for the
+ *  Security Light application
+ *  @author Chris Silvano, Alex Spradlin, Casey Layne */
+public class TestSecurityControl {
+
+	//used to determine which state you are in
+	LightControllerStateMachineObserverInterface lightStateMachineInterface;
+	LightControllerStateMachine lightStateMachine;
+	
+	/** This method
+	 *  - author Chris Silvano */
+	@Test 
+	public void securityAlarmTrippedNight() {
+		
+		//initial test of program being in correct initial state
+		assertEquals(lightStateMachine.getCurrentState(), lightStateMachineInterface.LAMP_ON_FULL_BRIGHTNESS);
+
+	}
+	
+	/** This method
+	 *  - author Chris Silvano */
+	@Test 
+	public void securityAlarmTrippedMotion() {
+
+	}
+	
+	/** This method
+	 *  - author Chris Silvano */
+	@Test 
+	public void alarmClearedLampOn() {
+
+	}
+	
+	/** This method
+	 *  - author Chris Silvano */
+	@Test 
+	public void alarmClearedLampOff() {
+
+	}
+		
+}
