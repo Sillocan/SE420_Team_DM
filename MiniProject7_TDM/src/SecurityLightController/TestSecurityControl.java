5f66aaf0de42d5797de1d830effd8a7d6dd97b5c
Chris Silvano <chris.silvano@hotmail.com>
2015-04-05 22:55:08 -0400

    Modified TestSecurityControl.java in an attempt to try a test case.

--- MiniProject7_TDM/src/SecurityLightController/TestSecurityControl.java
+++ MiniProject7_TDM/src/SecurityLightController/TestSecurityControl.java
@@ -1,7 +1,12 @@
 package src.SecurityLightController;
 
 import static org.junit.Assert.assertEquals;
+
+import javax.swing.JFrame;
+
+import org.junit.Before;
 import org.junit.Test;
+
 import src.SecurityLightController.*;
 import src.UI.*;
 
@@ -11,19 +16,50 @@ import src.UI.*;
 public class TestSecurityControl {
 
 	//used to determine which state you are in
-	LightControllerStateMachineObserverInterface lightStateMachineObserver;
+	LightControllerStateMachineObserverInterface lightStateMachineObserverInterface;
 	LightControllerStateMachine lightStateMachine;
 	LightTimerInterface lightTimerInterface;
 	LightDeviceInterface lightDeviceInterface;
+	TestStateMachineObserver testStateMachineObserver;
+	
+	@Before
+	public void setUpTests(){
+		testStateMachineObserver = new TestStateMachineObserver();
+		lightStateMachine = new LightControllerStateMachine();
+		
+		//subscribe for notifications
+		lightStateMachine.subscribe(testStateMachineObserver);
+		
+		JFrame frame = new JFrame("Security Light Controller GUI");
+		SecurityLampSimulatedUI cwrb1 = new SecurityLampSimulatedUI(lightStateMachine);
+		frame.setContentPane(cwrb1);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.setBounds(300, 0, 20, 10);
+		frame.pack();
+		frame.setVisible(true);
+
+		lightStateMachine.setLight(cwrb1);
+		lightStateMachine.setTmr(new LightTimer(lightStateMachine));
+	}
+	
+	/** Test initial boot-up of system into correct state */
+	@Test
+	public void initialStartUp(){
+		
+		//doesn't pass without this line...
+		//it shouldn't need this line
+		//testStateMachineObserver.updateLightState(TestStateMachineObserver.LAMP_OFF_DAYLIGHT);
+		
+		testStateMachineObserver.assertToTestValidState();
+		
+		assertEquals(testStateMachineObserver.currentState, TestStateMachineObserver.LAMP_OFF_DAYLIGHT);
+	}
 	
 	/** This method
 	 *  - author Chris Silvano */
 	@Test 
 	public void securityAlarmTrippedNight() {
-		
-		//initial test of program being in correct initial state
-		//this returns null pointer right now...
-		assertEquals(lightStateMachine.getCurrentState(), lightStateMachineObserver.LAMP_ON_FULL_BRIGHTNESS);
 
 	}
 	
